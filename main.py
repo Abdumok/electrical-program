a75dc83640bb65e6ecdac3db634aae011fcb0d21
Abdullahi Mohammmed Ali <abdumok19@gmail.com>
2025-03-11 23:43:17 +0300

    initial commit, setup main screen

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+BACKGROUND = "#A6CDC6"
+
+
+window= Tk()
+window.title("Project X")
+window.config(padx= 20, pady= 20, bg=BACKGROUND)
+window.minsize(width=800, height=500)
+
+
+window.mainloop()
